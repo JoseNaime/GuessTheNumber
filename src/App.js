b9a3bedcacd8f201eef7f4646d0b856e601767f9
JoseNaime <josep.naimeg@gmail.com>
2021-02-03 13:27:43 -0600

    feat: Google Analytics

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import GameScreen from "./screens/GameScreen";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import HomeScreen from "./screens/HomeScreen";
+import ReactGA from 'react-ga';
 
 const App = () => {
+    ReactGA.initialize('G-ZK5PC3NQ91'); // Aqui pones tu identificador
     return (
         <div>
             <Router>
                 <Switch>
                     <Route exact path="/game">
                         <GameScreen/>
+                        {ReactGA.pageview("/game")}
                     </Route>
                     <Route path="/">
                         <HomeScreen/>
+                        {ReactGA.pageview("/home")}
                     </Route>
                 </Switch>
             </Router>
