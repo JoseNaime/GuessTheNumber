3ac5cc1948a05e748194f1b3d789597c9920f3e5
JoseNaime <josep.naimeg@gmail.com>
2021-02-03 13:36:34 -0600

    fix: Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import GameScreen from "./screens/GameScreen";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import HomeScreen from "./screens/HomeScreen";
 import ReactGA from 'react-ga';
 
+
+ReactGA.initialize('G-ZK5PC3NQ91');
 const App = () => {
-    ReactGA.initialize('G-ZK5PC3NQ91'); // Aqui pones tu identificador
+
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search)
+    }, [])
+
     return (
         <div>
             <Router>
                 <Switch>
                     <Route exact path="/game">
                         <GameScreen/>
-                        {ReactGA.pageview("/game")}
                     </Route>
                     <Route path="/">
                         <HomeScreen/>
-                        {ReactGA.pageview("/home")}
                     </Route>
                 </Switch>
             </Router>
