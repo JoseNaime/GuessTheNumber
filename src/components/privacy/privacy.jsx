558c1a23aa190a34ceac217ab78000dc2061b2e1
JoseNaime <josep.naimeg@gmail.com>
2021-02-13 11:15:21 -0600

    feat: privacy policy

--- src/components/privacy/privacy.jsx
+++ src/components/privacy/privacy.jsx
@@ -7,7 +7,7 @@ function Privacy(props) {
         <section id="privacy" className={!isVisible ? 'hide' : '' } >
             <div className="wrapper">
                 <h2>Privacy Policy Term</h2>
-                <p className={'smallText'}>This Web collects some Personal Data from its Users.<a href={'https://www.iubenda.com/privacy-policy/25783020'} target="_blank">Privacy Policy</a></p>
+                <p className={'smallText'}>This Web collects some Personal Data from its Users.<a href={'https://www.iubenda.com/privacy-policy/25783020'} rel="noopener noreferrer" target="_blank">Privacy Policy</a></p>
                 <div className="right">
                     <button onClick={() => window.close()}>Decline</button>
                     <button onClick={() => setIsVisible(false)}>Accept</button>
